f3c997c379c6e51ebb8c9be2d96d4ad2c60d59b7
Jithin A P <jithinap009@gmail.com>
2020-11-01 09:27:51 +0530

    Added registration_screen.dart

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String ID = 'RegistrationScreen';
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
