4fa91e0617c58db9a7207d5afa6f6bace3ac8053
Jithin A P <jithinap009@gmail.com>
2020-11-01 08:20:42 +0530

    Basic UI for login screen implemented

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
 
 const String LOGIN_INFO = "LOGIN_INFO";
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'hint',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,12 @@
+import 'package:attendance/components/rounded_button.dart';
+import 'package:attendance/constants.dart';
+import 'package:attendance/screens/home_screen.dart';
 import 'package:attendance/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String ID = 'LoginScreen';
@@ -10,8 +16,13 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   FlutterSecureStorage storage;
+  String email;
+  String password;
+  bool showSpinner = false;
+
   @override
   void initState() {
+    Firebase.initializeApp();
     storage = FlutterSecureStorage();
     super.initState();
   }
@@ -20,13 +31,78 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: FlatButton(
-          child: Text('Login'),
-          onPressed: () async{
-            await storage.write(key: LOGIN_INFO, value: 'LOGGED_IN');
-          },
-        )
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Log In',
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final _user =
+                    FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    if (_user != null) {
+                      Navigator.pushNamed(context, HomeScreen.ID);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    // TODO : Implement all edge cases for error catching
+                    print(e);
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
