d9994bbf5a6b775301d7ea04ae0afe5ca6d58caa
Jithin A P <jithinap009@gmail.com>
2020-11-13 19:47:59 +0530

    ListTile improved

--- lib/components/subject_tile.dart
+++ lib/components/subject_tile.dart
@@ -10,15 +10,40 @@ class SubjectTile extends StatelessWidget {
     this.attendedClasses,
     this.totalClasses,
   });
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50.0,
+      margin: EdgeInsets.all(8.0),
+      height: 60.0,
       width: double.infinity,
-      color: Colors.pink,
-      child: Text(
-        title,
-        style: TextStyle(fontSize: 20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4.0,
+            spreadRadius: 4.0,
+            offset: Offset(
+              4.0,
+              2.0,
+            ),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 25.0,
+          top: 15.0,
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
